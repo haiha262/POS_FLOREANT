4df4ac7c02ce685d5e918484953e0d27997eadf3
Ha Tran <haiha262@gmail.com>
2019-03-09 10:13:27 +1100

    ver 105

--- database/derby-server/start-server.bat
+++ database/derby-server/start-server.bat
@@ -1,4 +1,4 @@
 set CLASSPATH=lib\derby.jar;lib\derbynet.jar
 
-java -cp %CLASSPATH% org.apache.derby.drda.NetworkServerControl start -h 192.168.20.11 -p 51527
+java -cp %CLASSPATH% org.apache.derby.drda.NetworkServerControl start -h 192.168.20.2 -p 51527
 pause
--- database/derby-server/start-server.sh
+++ database/derby-server/start-server.sh
@@ -1,3 +1,3 @@
-java -cp ./lib/derbynet.jar org.apache.derby.drda.NetworkServerControl start -h 192.168.20.11 -p 51527
+java -cp ./lib/derbynet.jar org.apache.derby.drda.NetworkServerControl start -h 192.168.20.2 -p 51527
 
 
